4cc0efcd630771e3ed862779fd2552bb33d8931c
Adeline Izri <adeline.rudnicki@gmail.com>
2022-11-01 20:08:03 +0100

    Ajout de commentaires

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // List de tout les composants, les directives, les pipes qui appartiennent au module
   declarations: [
     AppComponent
   ],
+  // Déclarations de tout les éléments dont on a besoin d'importer et qui sont d'autres modules
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  // Permet d'utiliser le sysyème d'injection d'angular
   providers: [],
+  // Il s'agit du composant racine. Permet de dire à angular quel est le premier composant qui doit démarer lorsque l'utilisateur charge l'application dans son navigateur
   bootstrap: [AppComponent]
 })
 export class AppModule { }
