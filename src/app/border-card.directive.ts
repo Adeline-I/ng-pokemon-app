ddd1e22f2eb97375cc9b54f1786e06f4f378d4aa
Adeline Izri <adeline.rudnicki@gmail.com>
2022-11-04 16:26:26 +0100

    Ajout de l'écouteur d'événement HostListener

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef } from '@angular/core';
+// HostListener : permet de lier une méthode de notre directive à un événement donné
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[pkmnBorderCard]'
@@ -11,14 +12,23 @@ export class BorderCardDirective {
     this.setBorder('#f5f5f5');
   }
 
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBorder('#009688');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder('#f5f5f5');
+  }
+
   // Création de méthodes
   setHeight(height: number) {
     // nativeElement permet d'accéder réellement à l'élément natif du Dom sur lequel la directive sera appelée
-    this.el.nativeElement.style.height = `${height}px`;
+    this.el.nativeElement.style.height = height + 'px';
   }
 
   setBorder(color: string) {
-    this.el.nativeElement.style.border =`solid 4x ${color}`;
+    let border = 'solid 4px ' + color;
+    this.el.nativeElement.style.border = border;
   }
 
 }
