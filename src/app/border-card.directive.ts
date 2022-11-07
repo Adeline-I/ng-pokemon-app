e3deafcc6a9163c724847706c783b08b9fd724fe
Adeline Izri <adeline.rudnicki@gmail.com>
2022-11-07 10:42:01 +0100

    Déclaration de propriétés

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -6,29 +6,34 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class BorderCardDirective {
 
+  // Déclaration de propriétés
+  private initialColor: string = '#f5f5f5';
+  private defaultColor: string = '#009688';
+  private defaultHeight: number = 180;
+
   // ElementRef est une référence vers l'élément du Dom sur lequel on va appliquer la directive
   constructor(private el: ElementRef) { 
-    this.setHeight(180);
-    this.setBorder('#f5f5f5');
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor);
   }
 
   @Input('pkmnBorderCard') borderColor: string;
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder(this.borderColor || '#009688');
+    this.setBorder(this.borderColor || this.defaultColor);
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
   }
 
   // Création de méthodes
-  setHeight(height: number) {
+  private setHeight(height: number) {
     // nativeElement permet d'accéder réellement à l'élément natif du Dom sur lequel la directive sera appelée
     this.el.nativeElement.style.height = height + 'px';
   }
 
-  setBorder(color: string) {
+  private setBorder(color: string) {
     let border = 'solid 4px ' + color;
     this.el.nativeElement.style.border = border;
   }
