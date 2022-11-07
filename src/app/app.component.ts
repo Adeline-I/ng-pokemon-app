68df763e293a24024ea53e1d14da5dd1ee78e726
Adeline Izri <adeline.rudnicki@gmail.com>
2022-11-07 12:09:53 +0100

    Transfert de la logique d'affichage de la liste des pokemons vers list-pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,14 @@
 // Un composant est une classe (logique du composant, propriétés et méthodes) + une vue (template)
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component } from '@angular/core';
 
 // Utilisation de l'importation Component sous la forme d'un décorateur avec @ qui va permettre de construire un composant web
 @Component({
   // selector permet de donner un nom (avec balise html personnalisée) au composant afin de l'identifier par la suite
   selector: 'app-root',
   // template permet de définir le code html
-  // template: `
-  //     <h1>
-  //       Liste de Pokémons
-  //     </h1>
-  // `,
   templateUrl:'./app.component.html',
   styles: []
 })
-export class AppComponent implements OnInit {
-  // déclaration de propriétés
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
+export class AppComponent {
 
-  // déclaration de méthodes
-  ngOnInit(): void {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string) {
-    // '+' équivaut à la conversion 'number'
-    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
-    if (pokemon) {
-      console.log(`Vous avez demandé le pokémon ${pokemon.name}.`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Vous avez demandé un pokémon qui n'existe pas.`);
-      this.pokemonSelected = pokemon;
-    }
-  }
 }
--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-list-pokemon',
   templateUrl: './list-pokemon.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class ListPokemonComponent implements OnInit {
 
+  // déclaration de propriétés
+  pokemonList: Pokemon[] = POKEMONS;
+  
   constructor() { }
 
   ngOnInit(): void {
