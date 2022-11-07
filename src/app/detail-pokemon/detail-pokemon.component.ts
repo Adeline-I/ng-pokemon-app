eb598352d207bcafe8b4622ababea1a62cf22d77
Adeline Izri <adeline.rudnicki@gmail.com>
2022-11-07 12:10:20 +0100

    Ajout de la logique d'affichage d'un pokemon par son id

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-detail-pokemon',
   templateUrl: './detail-pokemon.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class DetailPokemonComponent implements OnInit {
 
-  constructor() { }
+  // Ajout de propriétés
+  pokemonList: Pokemon[];
+  // pokemon courrent qui sera demandé dans l'url (id)
+  pokemon: Pokemon|undefined;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    // A l'initialisation, pokemonList va valoir POKEMONS
+    this.pokemonList = POKEMONS;
+
+    // router : accès au router
+    // snapshot : obtenir la donnée à l'instant T
+    // paramMap : tableau avec tout les paramètres de l'url à un instant T
+    const pokemonId: string|null = this.route.snapshot.paramMap.get('id');
+
+    if (pokemonId) {
+      this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    }
   }
 
 }
