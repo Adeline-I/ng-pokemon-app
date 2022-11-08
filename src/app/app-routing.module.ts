8e7aef1d007838a5e4639a9b446a6379f4e1c018
Adeline Izri <adeline.rudnicki@gmail.com>
2022-11-08 15:51:07 +0100

    Ajout d'un module pokemon et transfert de la gestion des pokemons vers celui-ci

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.component";
-import { ListPokemonComponent } from "./list-pokemon/list-pokemon.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
-    { path: 'pokemons', component: ListPokemonComponent },
-    { path: 'pokemon/:id', component: DetailPokemonComponent },
     // Ajout d'une route par défaut, ici, celle de la liste des pokemons
     // pathMatch permet d'éviter les effets de bord et donc de mieux isoler les choses
     { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
@@ -15,7 +11,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ]
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @NgModule({
   // List de tout les composants, les directives, les pipes qui appartiennent au module
   declarations: [
     AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
     PageNotFoundComponent
   ],
   // Déclarations de tout les éléments dont on a besoin d'importer et qui sont d'autres modules
   imports: [
     BrowserModule,
+    PokemonModule,
     AppRoutingModule
   ],
-  // Permet d'utiliser le sysyème d'injection d'angular
-  providers: [],
+  // Permet d'utiliser le système d'injection d'angular
+  providers: [
+
+  ],
   // Il s'agit du composant racine. Permet de dire à angular quel est le premier composant qui doit démarer lorsque l'utilisateur charge l'application dans son navigateur
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
