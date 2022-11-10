ff5b87c390b403a159fbe28e6e351f4f6fdea6cb
Adeline Izri <adeline.rudnicki@gmail.com>
2022-11-10 11:52:23 +0100

    Modification du service suite à l'utilisation de requêtes pour récupérer les données

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { Pokemon } from './pokemon';
 
 @Injectable(
@@ -9,14 +10,40 @@ import { Pokemon } from './pokemon';
 )
 export class PokemonService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  getPokemonList(): Pokemon[] {
-    return POKEMONS;
+  // On va recevoir une donnée qui va arriver dans le temps qui contient un tableau de pokémons
+  // Suptilité synchrone / asynchrone : on ne retourne pas directement les pokémons car on ne les a pas à l'instant T, on va retourner un flux (Observable) qui lui contiendra les pokémons et qui arriveront plus tard dans le temps
+  // On ne retourne plus une constante mais un flux qui contient les pokémons
+  getPokemonList(): Observable<Pokemon[]> {
+    // http renvoi des flux que l'on peut typer
+    // On fait une requête http.get et on va recevoir un observable
+    // On spécifie que la réponse contient une liste de Pokemon[]
+    // En paramètre de la méthode get, on passe une url vers l'Api
+    return this.http.get<Pokemon[]>('api/pokemons').pipe(
+      // on définit ce que l'on veut faire en plus du traitement de la requête
+      // loguer la réponse et intercepter les erreurs éventuelles
+      // tap équivaut à un console.log pour un Observable
+      tap((pokemonList) => console.table(pokemonList)),
+      catchError((error) => {
+        console.log(error);
+        // On retourne un observable : tableau vide
+        return of([]);
+      })
+      // Rend à disposition l'objet lorsque les composants vont l'appeller, qui pourront 'subscribe' pour récupérer les données qui sont contenus dans le flux 
+    );
   }
 
-  getPokemonById(pokemonId: number): Pokemon|undefined {
-    return POKEMONS.find(pokemon => pokemon.id == pokemonId);
+  getPokemonById(pokemonId: number): Observable<Pokemon|undefined> {
+    return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
+      tap((pokemon) => console.log(pokemon)),
+      catchError((error) => {
+        console.log(error);
+        return of(undefined);
+      })
+    );
   }
 
   getPokemonTypeList(): string[] {
